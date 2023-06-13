b71db67c23c60c99388b8a9015e577e9bd9289a2
jorgeEF <jorgefemenia@gmail.com>
2023-06-13 20:12:46 -0300

    lista elementos dinamicos

--- next12-udemycourse-2022/pages/list/[details].js
+++ next12-udemycourse-2022/pages/list/[details].js
@@ -1,11 +1,17 @@
-
+import { useRouter } from "next/router"
 
 
 export default function ListDetails(){
+    // uso router para acceder al query de la ruta dinamica para filtrar elemento
+    const router = useRouter();
+    const {query} = router;
+    const {details} = query;
+
+    //console.log(router);
 
     return (
         <div>
-            <h1>Componente Detalles de Lista - ruta dinámica</h1>
+            <h1>Componente Detalles de Lista del elemento {details}</h1>
         </div>
     )
 }
--- next12-udemycourse-2022/pages/list/index.js
+++ next12-udemycourse-2022/pages/list/index.js
@@ -1,11 +1,22 @@
+import Link from "next/link"
 
 
-
-export default function List(){
+export default function List() {
 
     return (
         <div>
             <h1>Componente Lista</h1>
+            <ul>
+                <Link href="/list/1">
+                    <li>Lista 1</li>
+                </Link>
+                <Link href="/list/2">
+                    <li>Lista 2</li>
+                </Link>
+                <Link href="/list/3">
+                    <li>Lista 3</li>
+                </Link>
+            </ul>
         </div>
     )
 }
