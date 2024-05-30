4384186e190676d3fb5253c4b1ad8227c494284c
JFEMENIA <jorgefemenia@gmail.com>
2024-05-30 00:41:58 -0300

    avances edit user

--- fullstack-crud-sqlz/app/users/edit/[id]/page.tsx
+++ fullstack-crud-sqlz/app/users/edit/[id]/page.tsx
@@ -3,22 +3,24 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { User } from '@/utils/types';
+import { useParams } from 'next/navigation';
 
-async function loadUser(): Promise<User> {
-  const { data } = await axios.get('/api/users/1');
+async function loadUser(id: string): Promise<User> {
+  const { data } = await axios.get(`/api/users/${id}`);
   return data;
 }
 
 export default function UsersPage() {
   const [user, setUser] = useState<User>();
+  const { id } = useParams();
 
   useEffect(() => {
     async function fetchUser() {
-      const userData = await loadUser();
+      const userData = await loadUser(id[0]);
       setUser(userData);
     }
     fetchUser();
-  }, []);
+  }, [id]);
 
   return (
     <div className="container mx-auto px-4 min-h-screen flex items-center justify-center">      
