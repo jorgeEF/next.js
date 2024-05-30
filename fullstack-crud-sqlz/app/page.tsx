f61d72a5a7e56924405ec2491c9094eb86408050
JFEMENIA <jorgefemenia@gmail.com>
2024-05-30 15:55:51 -0300

    quitado crud nextjs 14, crud en otro repo

--- fullstack-crud-sqlz/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Link from "next/link";
-
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">       
-            <Link href={'/users'}> <button type="button" className="bg-green-800 hover:bg-green-700 text-white rounded p-1">ir a CRUD de usuarios</button></Link>                     
-    </main>
-  );
-}
